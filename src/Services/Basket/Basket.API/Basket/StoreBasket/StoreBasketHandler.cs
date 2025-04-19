015b9c26713fe08f6f51605b213a181349dc4653
sunil <sunill_waugh@yahoo.com>
2025-04-19 12:13:54 -0700

    Completed Upto lecture 111

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreaBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreaBasketCommandValidator : AbstractValidator<StoreaBasketCommand>
+    {
+        public StoreaBasketCommandValidator()
+        {
+
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+        public class StoreBasketCommandHandler
+            : ICommandHandler<StoreaBasketCommand, StoreBasketResult>
+        {
+            public async Task<StoreBasketResult> Handle(StoreaBasketCommand command, CancellationToken cancellationToken)
+            {
+                ShoppingCart cart = command.Cart;
+
+                //Todo = store basket in database
+                //Todo = update cache
+
+                return new StoreBasketResult("sdw");
+            }
+        }
+    }
+}
