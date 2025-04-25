67212f18ed98f5d010602855ae5950c648f3afa1
sunil <sunill_waugh@yahoo.com>
2025-04-25 10:53:50 -0700

    Completed upto Lecture 141

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,29 +1,28 @@
-﻿
-
-namespace Basket.API.Basket.StoreBasket
+﻿namespace Basket.API.Basket.StoreBasket
 {
-    public record StoreaBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
 
-    public class StoreaBasketCommandValidator : AbstractValidator<StoreaBasketCommand>
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
-        public StoreaBasketCommandValidator()
+        public StoreBasketCommandValidator()
         {
 
             RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
             RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
         }
-        public class StoreBasketCommandHandler
-            : ICommandHandler<StoreaBasketCommand, StoreBasketResult>
+        public class StoreBasketCommandHandler(IBasketRepository repository)
+            : ICommandHandler<StoreBasketCommand, StoreBasketResult>
         {
-            public async Task<StoreBasketResult> Handle(StoreaBasketCommand command, CancellationToken cancellationToken)
+            public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
             {
                 ShoppingCart cart = command.Cart;
 
                 //Todo = store basket in database
                 //Todo = update cache
+                await repository.StoreBasket(command.Cart, cancellationToken);
 
-                return new StoreBasketResult("sdw");
+                return new StoreBasketResult(command.Cart.UserName);
             }
         }
     }
