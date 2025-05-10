21325845a3cc316393412fbaeb1bbc8001f62f46
sunil <sunill_waugh@yahoo.com>
2025-05-10 04:22:48 -0700

    pushing upto lecture 195

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(typeof(DependencyInjection).Assembly);
+            //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+            //services.AddScoped<IValidator<Order>, OrderValidator>();
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            // Add your infrastructure services here
+            // services.AddDbContext<ApplicationDbContext>(options =>
+            // options.UseSqlServer(connectionString));
+            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            return services;
+        }
+    }
+}
