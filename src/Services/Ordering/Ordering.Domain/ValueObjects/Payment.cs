1e589ef9e76611130bcfb39813065ed90b8fa1ed
sunil <sunill_waugh@yahoo.com>
2025-05-10 23:10:39 -0700

    Pushing upto Lecture 199 Domain Layer Completed

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -11,5 +11,24 @@
         protected Payment()
         {
         }
+
+        private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
