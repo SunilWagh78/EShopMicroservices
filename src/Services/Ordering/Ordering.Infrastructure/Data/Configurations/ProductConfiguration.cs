1f3b61128b5037f8b4372d50c7af77f9f4bb84de
sunil <sunill_waugh@yahoo.com>
2025-05-24 10:43:22 +0530

    Completed upto lecture 210, EF Errors

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasConversion(
+                productId => productId.Value,
+                dbId => ProductId.Of(dbId) // Assuming you have a method to convert from string to CustomerId
+            );
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure
@@ -14,9 +9,9 @@ namespace Ordering.Infrastructure
         {
             var connectionString = configuration.GetConnectionString("Database");
             // Add your infrastructure services here
-            // services.AddDbContext<ApplicationDbContext>(options =>
-            // options.UseSqlServer(connectionString));
-            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
             return services;
         }
     }
